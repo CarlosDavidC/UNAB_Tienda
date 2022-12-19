bb575c2ccba958d082b457f7097c6ad1ff67ef3f
David <dcarreno144@gmail.com>
2022-12-19 16:03:09 -0500

    Commit final corrección

---
+++ src/main/java/com/unab/tienda/IService/IInventariosService.java
@@ -0,0 +1,17 @@
+package com.unab.tienda.IService;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.unab.tienda.Collection.Inventarios;
+
+public interface IInventariosService {
+
+	public List<Inventarios> all();
+
+	public Optional<Inventarios> findById(String id);
+
+	public Inventarios save(Inventarios productos);
+
+	public void delete(String id);
+}
